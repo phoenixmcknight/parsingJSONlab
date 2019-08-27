c5df6273fab0c09948a0198d07c3c736b5eda1f1
phoenixmcknight <52206805+phoenixmcknight@users.noreply.github.com>
2019-08-27 14:10:54 -0400

    Add files via upload

---
+++ parsingJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parsingJSON
+//
+//  Created by Phoenix McKnight on 8/27/19.
+//  Copyright © 2019 Phoenix McKnight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
